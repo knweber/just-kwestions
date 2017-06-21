3d927f868ec1417ccb16c5f0ba04bddb927501ec
Jordan Kamin <jordanakamin@gmail.com>
2017-06-21 11:01:56 -0700

    Writes empty describes for QuestionsController

---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,16 @@
+describe 'QuestionsController' do
+  describe 'get "/questions"' do
+  end
+  describe 'get "/questions/:id"' do
+  end
+  describe 'get "/questions/new"' do
+  end
+  describe 'post "/questions"' do
+  end
+  describe 'get "/questions/:id/edit"' do
+  end
+  describe 'put "/questions/:id"' do
+  end
+  describe 'delete "/questions/:id"' do
+  end
+end
