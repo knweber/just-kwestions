830812da296798918334bda94bfc1ceddd157421
Jordan Kamin <jordanakamin@gmail.com>
2017-06-21 12:21:05 -0700

    builds out test suite for questions_controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,47 +1,114 @@
 describe 'QuestionsController' do
+  let(:question) { Question.create(prompt: 'Why so much ham?')}
+  let(:new_question) { Question.new(prompt: 'Is it really worth it?') }
+  let(:response) { Response.creat(content: 'because ham is fun to say.', question_id: question.id)}
   describe 'get "/questions"' do
-    it 'responds with a status of 200' do
+    before(:each) do
+      get '/questions'
     end
-    it 'has a question in the response body'
+    it 'responds with a status of 200' do
+      expect(last_response.status).to eq(200)
+    end
+    it 'has a question in the response body' do
+      expect(last_response.body).to include(question.prompt)
+    end
   end
   describe 'get "/questions/:id"' do
-    it 'responds with a status of 200'
-    it 'has a question in the response body'
-    it 'has the associated question responses in the response body'
+    before(:each) do
+      get '/questions', {id: 1}
+    end
+    it 'responds with a status of 200' do
+      expect(last_response.status).to eq(200)
+    end
+    it 'has a question in the response body' do
+      expect(last_response.body).to include(question.prompt)
+    end
+    it 'has the associated question responses in the response body' do
+      expect(last_response.body).to include(question.answers[1].contest)
+    end
   end
   describe 'get "/questions/new"' do
-    it 'responds with a status of 200'
-    it 'has a question in the response body'
+    before(:each) do
+      get '/questions/new'
+    end
+    it 'responds with a status of 200' do
+      expect(last_response.status).to eq(200)
+    end
+    it 'has a question form in the response body' do
+      expect(last_response.body).to include('Make a new question!')
+    end
   end
   describe 'post "/questions"' do
     context 'on success' do
-      it 'responds with a status of 302'
-      it 'responds with the newly created question in the response body'
-      it 'adds the new question to the database'
+      before(:each) do
+        post '/questions', {question: {prompt: new_question.prompt}}
+      end
+      it 'responds with a status of 302' do
+        expect(last_response.status).to eq(302)
+      end
+      it 'responds with the newly created question in the response body' do
+        expect(last_response).to include(new_question.prompt)
+      end
+      it 'adds the new question to the database' do
+        expect(Question.last).to eq(new_question)
+      end
     end
     context 'on failure' do
-      it 'responds with a status of 200'
-      it 'responds with errors in the response body'
-      it 'adds errors to the question object'
+      before(:each) do
+        post '/questions', {}
+      end
+      it 'responds with a status of 200' do
+        expect(last_response.status).to eq(200)
+      end
+      it 'responds with errors in the response body' do
+        expect(last_response.body).to include("can't be blank")
+      end
     end
   end
-  describe 'get "/questions/:id/edit"' do
-    it 'repsonds with a status of 200'
-    it 'responds with the edit page in the response body'
+  describe 'get "questions/:id/edit"' do
+    before(:each) do
+      get "questions/#{question.id}/edit"
+    end
+    it 'responds with a status of 200' do
+      expect(last_response.status).to eq(200)
+    end
+    it 'responds with the edit page in the response body' do
+      expect(last_response.body).to include('Edit your question')
+    end
   end
-  describe 'put "/questions/:id"' do
+  describe 'put "questions/:id"' do
     context 'on success' do
-      it 'responds with a status of 302'
-      it 'responds with the newly updated question'
+      before(:each) do
+        put "/questions/#{question.id}", {prompt: 'Big ol sammich'}
+      end
+      it 'responds with a status of 302' do
+        expect(last_response.status).to eq(302)
+      end
+      it 'responds with the newly updated question' do
+        expect(last_response.body).to include('sammich')
+      end
     end
     context 'on failure' do
-      it 'responds with a status of 200'
-      it 'responds with the question edit form containing errors in the response'
-      it 'adds errors to the question object'
+      before(:each) do
+        put "/questions/#{question.id}"
+      end
+      it 'responds with a status of 200' do
+        expect(last_response.status).to eq(200)
+      end
+      it 'responds with the question edit form containing errors in the response' do
+        expect(last_response.body).to include("can't be blank")
+      end
     end
   end
   describe 'delete "/questions/:id"' do
-    it 'responds with a status of 302'
-    it' responds with the index page without the deleted question in the response body'
+    before(:each) do
+      delete "questions/#{question.id}"
+    end
+    it 'responds with a status of 302' do
+      expect(last_response.status).to eq(302)
+    end
+    it' responds with the index page without the deleted question in the response body' do
+      expect(last_response.body).to_not include(question.prompt)
+    end
   end
 end
