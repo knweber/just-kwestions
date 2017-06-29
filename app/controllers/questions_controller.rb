34ac67b49e6e33e45eddb18b07260f867f1e3cbc
Jarred Taylor & Kala Wetzler <pair+jarredt.kala-lee@devbootcamp.com>
2017-06-29 15:12:08 -0700

    Add authors, change migration fields to text, seed comments

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 get '/questions/:id' do
   @question = Question.find(params[:id])
   @answers = @question.answers
+  @question_comments = @question.comments
   erb :'questions/show'
 end
--- db/migrate/20170629115440_create_answers.rb
+++ db/migrate/20170629115440_create_answers.rb
@@ -1,7 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
-      t.string  :answer, null:false
+      t.string  :text, null:false
       t.integer :question_id, null:false
       t.string  :user_id, null:false
 
