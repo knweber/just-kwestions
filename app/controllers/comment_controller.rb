cbdf0e276d3df82988e66c3b405bbd9bfab2445d
Kris & Jeremy Lee <pair+knweber.pyrofist@devbootcamp.com>
2017-06-29 16:56:35 -0700

    Create question comments routes and views

---
+++ app/controllers/comment_controller.rb
@@ -0,0 +1,14 @@
+get '/questions/:question_id/comments/new' do
+  erb :'comments/new', locals: { action: "questions/#{params[:question_id]}/comments" }
+end
+
+post '/questions/:question_id/comments' do
+  question = Question.find(params[:question_id])
+  comment = question.comments.create(text: params[:text], user_id: User.all.sample.id)
+
+  if comment.valid?
+    redirect "/questions/#{question.id}"
+  else
+    erb :'comments/new', locals: { action: "questions/#{params[:question_id]}/comments", errors: comment.errors.full_messages }
+  end
+end
