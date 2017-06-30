ecac1d7da65bdf8346f5263af3d07a61f3ece788
Jarred Taylor & Kris <pair+jarredt.knweber@devbootcamp.com>
2017-06-30 11:46:03 -0700

    Finish rewriting routes and add tests for answer comments

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -16,19 +16,19 @@ post '/questions/:question_id/comments' do
 end
 
 # Comments on answers
-get '/questions/:question_id/answers/comments/new' do
-  erb :'comments/new', locals: { action: "questions/#{params[:question_id]}/answers/comments" }
+get '/questions/:question_id/answers/:answer_id/comments/new' do
+  erb :'comments/new', locals: { action: "questions/#{params[:question_id]}/answers/#{params[:answer_id]}/comments" }
 end
 
-post '/questions/:question_id/answers/comments' do
+post '/questions/:question_id/answers/:answer_id/comments' do
   question = Question.find(params[:question_id])
-  answer = Answer.find(params[:question_id])
+  answer = Answer.find(params[:answer_id])
   comment = answer.comments.create(text: params[:text], user_id: User.all.sample.id)
 
   if comment.valid?
     redirect "/questions/#{question.id}"
   else
     status 422
-    erb :'comments/new', locals: { action: "questions/#{params[:question_id]}/answers/comments", errors: comment.errors.full_messages }
+    erb :'comments/new', locals: { action: "questions/#{params[:question_id]}/answers/#{params[:answer_id]}/comments", errors: comment.errors.full_messages }
   end
 end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
 
 describe "Comment Controller" do
-  let(:question){ Question.create(text: "this is a question?", user_id: 1)}
+  let!(:question){ Question.create(text: "this is a question?", user_id: 1)}
+  let!(:answer) { Answer.create(text: 'yes, it is!', question_id: question.id, user_id: 1)}
+
+# Adding comments to questions
   context 'get /questions/:question_id/comments/new route' do
     it "should respond to the /questions/:question_id/comments/new route" do
       get "/questions/1/comments/new"
@@ -21,7 +24,7 @@ describe "Comment Controller" do
       expect(last_response.status).to eq(302)
     end
 
-    it 'should redirect to /questions/:question_id if valid question is given' do
+    it 'should redirect to /questions/:question_id if valid comment is given' do
       post "/questions/1/comments", {"text"=>"comment"}
       expect(last_response.location).to include("/questions/1")
     end
@@ -42,4 +45,47 @@ describe "Comment Controller" do
       expect(last_response.body).to include("Text can't be blank")
     end
   end
+
+# Adding comments to answers
+  context 'get /questions/:question_id/answers/:answers_id/comments/new route' do
+    it "should respond to the /questions/:question_id/answers/:answers_id/comments/new route" do
+      get "/questions/1/answers/1/comments/new"
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'should display a form' do
+      get "/questions/1/answers/1/comments/new"
+      expect(last_response.body).to include('<form id=\'comment-form\'')
+    end
+  end
+
+  context 'post /questions/:question_id/answers/:answer_id/comments route' do
+    let!(:user){ User.create(username: "testuser") }
+    it 'should redirect if valid comment is given' do
+      post "/questions/#{question.id}/answers/#{answer.id}/comments", {"text"=>"comment"}
+      expect(last_response.status).to eq(302)
+    end
+
+    it 'should redirect to /questions/:question_id if valid comment is given' do
+      post "/questions/#{question.id}/answers/#{answer.id}/comments", {"text"=>"comment"}
+      expect(last_response.location).to include("/questions/#{question.id}")
+    end
+
+    it 'will create a comment in the database if a valid comment is given' do
+       comment_count = answer.comments.all.count
+       post "/questions/#{question.id}/answers/#{answer.id}/comments", {"text"=>"comment"}
+       expect(answer.comments.all.count).to eq (comment_count + 1)
+     end
+
+    it 'should 422 if an invalid comment is given' do
+      post "/questions/#{question.id}/answers/#{answer.id}/comments", {"text"=>""}
+      expect(last_response.status).to eq(422)
+    end
+
+    it 'should include the error message' do
+      post "/questions/#{question.id}/answers/#{answer.id}/comments", {"text"=>""}
+      expect(last_response.body).to include("Text can't be blank")
+    end
+  end
+
 end
