07fc2e38e9667349a9efea67fe8c51b84c3dfe3d
Jarred Taylor & Kris <pair+jarredt.knweber@devbootcamp.com>
2017-06-30 16:14:29 -0700

    Made comment model votable, refactored upvote route

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,15 @@
-post '/questions/:question_id/votes' do
-  question = Question.find(params[:question_id])
-  question.votes.create(user_id: User.all.sample.id)
-  redirect "/questions/#{question.id}"
+post '/votes' do
+  if params[:voteable_type] == 'question'
+    voteable = Question.find(params[:voteable_id])
+  elsif params[:voteable_type] == 'answer'
+    voteable = Answer.find(params[:voteable_id])
+  else
+    voteable = Comment.find(params[:voteable_id])
+  end
+  voteable.votes.create(user_id: User.all.sample.id, upvote: params[:upvote])
+
+  redirect "/questions/#{params[:question_page]}"
+  
 end
 
 delete '/questions/:question_id/votes/:id' do
