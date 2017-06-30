5f42c470f2231bb24c0c33ac64f0d4ff55f89731
Kala Wetzler <kala.wetzler@gmail.com>
2017-06-30 07:58:24 -0700

    Update user model and migration for login/registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,33 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  include BCrypt
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }, uniqueness: true
+  # will this catch/throw an error at the registration level? \/
+  validates :password_hash, presence: true, length: { minimum: 8 }
+
   has_many :questions
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email, password)
+    @user = User.find_by_email(email)
+    return nil if @user == nil
+
+    if @user.password == password
+      @user
+    else
+      nil
+    end
+  end
+
 end
--- db/migrate/20170629112007_create_users.rb
+++ db/migrate/20170629112007_create_users.rb
@@ -2,6 +2,9 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username, null:false, unique:true
+      t. string :email, null:false, unique:true
+      t.string :password_hash, null:false
+      
       t.timestamps
     end
   end
