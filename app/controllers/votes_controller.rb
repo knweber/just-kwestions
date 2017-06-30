1f64d6f5b2c4c8989b27edab6db65fa9456e62a4
Jarred Taylor & Kris <pair+jarredt.knweber@devbootcamp.com>
2017-06-30 16:44:26 -0700

    add upvote and downvote functions for questions, answers, and comments

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -9,11 +9,11 @@ post '/votes' do
   voteable.votes.create(user_id: User.all.sample.id, upvote: params[:upvote])
 
   redirect "/questions/#{params[:question_page]}"
-  
+
 end
 
-delete '/questions/:question_id/votes/:id' do
-  question = Question.find(params[:question_id])
-    question.votes.delete(Vote.find_by(voteable_id: question.id, voteable_type: "Question"))
-    redirect "/questions/#{question.id}"
-end
+# delete '/questions/:question_id/votes/:id' do
+#   question = Question.find(params[:question_id])
+#     question.votes.delete(Vote.find_by(voteable_id: question.id, voteable_type: "Question"))
+#     redirect "/questions/#{question.id}"
+# end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,7 @@ class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
   has_many :comments, as: :commentable
+  has_many :votes, as: :voteable
 
   validates :text, presence: true
 end
