c060812862633df80186909246241c2d554c9316
Jarred Taylor & Kris <pair+jarredt.knweber@devbootcamp.com>
2017-06-30 15:39:01 -0700

    Change URL for answer comment

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -21,19 +21,3 @@ post '/comments' do
     erb :'comments/new', locals: { action: "comments", errors: comment.errors.full_messages, commentable_type: params[:commentable_type], commentable_id: params[:commentable_id]}
   end
 end
-
-# Comments on answers
-
-#
-# post '/questions/:question_id/answers/:answer_id/comments' do
-#   question = Question.find(params[:question_id])
-#   answer = Answer.find(params[:answer_id])
-#   comment = answer.comments.create(text: params[:text], user_id: User.all.sample.id)
-#
-#   if comment.valid?
-#     redirect "/questions/#{question.id}"
-#   else
-#     status 422
-#     erb :'comments/new', locals: { action: "questions/#{params[:question_id]}/answers/#{params[:answer_id]}/comments", errors: comment.errors.full_messages }
-#   end
-# end
