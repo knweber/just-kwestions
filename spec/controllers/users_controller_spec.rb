5662c63829b47c9dd3214907805edf6716ad8b97
Kala Wetzler & Jeremy Lee <pair+kala-lee.pyrofist@devbootcamp.com>
2017-07-02 12:52:04 -0700

    Finish tests for post users route

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -13,20 +13,27 @@ describe 'user controller' do
   end
 
   context 'post /users route' do
+    let!(:user_params) { { 'user' => { 'username' => 'Terry', 'email' => 'terry@gmail.com', 'password' => '12345678' } } }
     it 'redirects with valid user info' do
-      
+      post '/users', user_params
+      expect(last_response.status).to eq 302
     end
 
     it 'redirects to / with valid user info' do
-
+      post '/users', user_params
+      # COME BACK TO THIS IF TIME PERMITS
+      expect(last_response.location).to include('/')
     end
 
     it 'creates a user with valid user info' do
-
+      user_count = User.all.count
+      post '/users', user_params
+      expect(User.all.count).to eq (user_count +1)
     end
 
     it 'displays errors with invalid user info' do
-
+      post '/users', { 'user' => { 'username' => 'Terry', 'email' => 'terry@gmail.com', 'password' => '123456' } }
+      expect(last_response.body).to include('provide password of at least 8')
     end
 
   end
