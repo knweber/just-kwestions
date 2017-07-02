47554f106518dd5d053ce98485159d76931a4e9a
Kala Wetzler & Jeremy Lee <pair+kala-lee.pyrofist@devbootcamp.com>
2017-07-02 12:26:43 -0700

    Fill out tests for get users/new route

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,17 +2,19 @@ describe 'user controller' do
 
   context 'get /users/new' do
       it 'handles the route' do
-
+        get '/users/new'
+        expect(last_response.status).to eq 200
       end
 
       it 'displays a form' do
-
+        get '/users/new'
+        expect(last_response.body).to include('form id="session_form"')
       end
   end
 
   context 'post /users route' do
     it 'redirects with valid user info' do
-
+      
     end
 
     it 'redirects to / with valid user info' do
@@ -20,7 +22,7 @@ describe 'user controller' do
     end
 
     it 'creates a user with valid user info' do
-      
+
     end
 
     it 'displays errors with invalid user info' do
