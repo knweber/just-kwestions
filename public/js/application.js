969cacd41f645cd891d8d2eb69017b8376b9b56e
Jarred Taylor & Jeremy Lee <pair+jarredt.pyrofist@devbootcamp.com>
2017-07-09 14:51:12 -0700

    Refactor answer with partial

--- public/js/application.js
+++ public/js/application.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     e.preventDefault();
     $("#question-comment-form-container").toggle();
   });
+
   $("#new-question-comment-container .comment-form").on("submit", function(e) {
     e.preventDefault();
     var request = $.ajax({
@@ -19,6 +20,23 @@ $(document).ready(function() {
     });
     request.fail( function(data) {
       $("#question-comment-errors").html(data.responseText);
+    });
+  });
+
+  $("#new-answer-container a").on("click", function(e) {
+    e.preventDefault();
+    $("#answer-form-container").toggle();
+  });
+
+  $("#answer-form").on("submit", function(e) {
+    e.preventDefault();
+    var request = $.ajax({
+      url: this.action,
+      method: "POST",
+      data: $(this).serialize()
+    });
+    request.done( function(data) {
+      
     });
   });
 });
