154129112489292e4b302c0a9f5c3780cf0d8bcd
Jarred Taylor & Jeremy Lee <pair+jarredt.pyrofist@devbootcamp.com>
2017-07-09 13:57:26 -0700

    Do all the AJAX for adding question comments and errors

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
-  $("#question-comment-container a").on("click", function(e) {
+  $("#new-question-comment-container a").on("click", function(e) {
     e.preventDefault();
+    $("#question-comment-form-container").toggle();
+  });
+  $("#new-question-comment-container .comment-form").on("submit", function(e) {
+    e.preventDefault();
+    var request = $.ajax({
+      url: '/comments',
+      method: 'POST',
+      data: $(this).serialize()
+    });
+    request.done( function(data) {
+      $("#question-comment-container").append(data);
+      $(".comment-form textarea").val('');
+      $("#question-comment-form-container").hide();
+    });
+    request.fail( function(data) {
+      $("#question-comment-errors").html(data.responseText);
+    });
   });
 });
